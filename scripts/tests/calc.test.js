925c3856da9bdbb5d4a08321c879416ea363bfd1
Helen Smith <5b64nnhi@students.codeinstitute.net>
2024-12-24 12:59:14 +0000

    FEAT: Add caluculator and functions

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,16 @@
+const addition = require("../calc");
+
+describe("Calculator", () => {
+    describe("Addition function", () => {
+
+    });
+    describe("Subtraction function", () => {
+
+    });
+    describe("Multiply function", () => {
+
+    });
+    describe("Division function", () => {
+
+    });
+})
