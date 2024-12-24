4127e56c2bf7f628899f8b5c797a85d7dbe53578
Helen Smith <5b64nnhi@students.codeinstitute.net>
2024-12-24 13:13:39 +0000

    FIX: move files to correct directories so that can run test as it wasn't finding the file which I had in the incorrect directory.

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -2,7 +2,9 @@ const addition = require("../calc");
 
 describe("Calculator", () => {
     describe("Addition function", () => {
-
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        })
     });
     describe("Subtraction function", () => {
 
